8c20bf5000e1e6054a232d6d20c2a0801d79fc32
MTthoas <92540288+MTthoas@users.noreply.github.com>
2022-12-23 02:03:08 +0100

    Header & drawer

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
+  theme: {
+    extend: {},
+    colors: {
+      blue_theme: '#272A57',
+      white: "#FFF",
+      dark_theme : '#18191A;'
+    }
+  },
+  plugins: [],
+}
