bdb5b446a57d2fcb2cafaab30453bbf3ea88453f
MtThoas <matthias.pecquery@gmail.com>
2023-02-08 12:10:57 +0100

    Footer done

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import Header from './components/header';
+import Footer from './components/footer';
 import React from 'react';
+import './index.css';
 
 
 function App() {
@@ -7,6 +9,9 @@ function App() {
 
         <div className="App mx-auto">
             <Header />
+            <div className="h-screen"> 
+            </div>
+            <Footer />
         </div>
         
     );
